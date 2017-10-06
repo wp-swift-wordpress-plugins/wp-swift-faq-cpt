9686e727eed157e703127256abc50c1dac6e4d71
Gary Swift <garyswiftmail@gmail.com>
2017-10-06 12:09:36 +0100

    Added debug message Debug info can be set with the shortcode attribute

--- public/partials/wp-swift-faq-taxonomy-public-display.php
+++ public/partials/wp-swift-faq-taxonomy-public-display.php
@@ -15,14 +15,6 @@ $question_categories = get_terms( array(
     'taxonomy' => 'question_category',
 ) );
 
-if ($debug): ?>
-	<div class="callout warning">
-		<h5>// to do</h5>
-		<p>Questions with a <code>// to do</code> are currently awaiting to be assigned answers.</p>
-		<small>This is temporary feature that will not be visible in production.<br>This can be turned off by changing the WYSIWYG shortcode from <code>[faq-cat to-do="true"]</code> to <code>[faq-cat]</code>.</small>
-	</div>
-<?php endif;
-
 foreach ($question_categories as $key => $question_category): ?>
 <div class="question-category">
 	<h3 class="faq-header"><a href="<?php echo get_term_link( $question_category->term_id );; ?>"><?php echo $question_category->name; ?></a></h3>
@@ -54,9 +46,9 @@ foreach ($question_categories as $key => $question_category): ?>
 			?>
 			<li class="question">
 				<a class="questions-link" href="<?php echo get_the_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a>
-				<?php if ($debug && empty( $post->post_content ) ) echo "<pre>// to do</pre>"; ?>
+				<?php if ($debug && empty( $post->post_content ) ) echo "<code>// to do</code>"; ?>
 				<?php if ($answers): ?>
-					<div class="question-answer"><?php echo get_the_content($post->ID); ?></div>
+					<div class="question-answer"><?php echo get_the_content($post->ID, ''); ?></div>
 				<?php endif ?>
 			</li>
 		
@@ -70,3 +62,11 @@ foreach ($question_categories as $key => $question_category): ?>
 	endforeach;
 	?>
 </div><!-- @end .question-category -->
+
+<?php if ($debug): ?>
+	<div class="callout warning">
+		<h5>// to do</h5>
+		<p>Questions with a <code>// to do</code> are currently awaiting to be assigned answers.</p>
+		<small>This is temporary feature that will not be visible in production.<br>This can be turned off by changing the WYSIWYG shortcode from <code>[faq-cat to-do="true"]</code> to <code>[faq-cat]</code>.</small>
+	</div>
+<?php endif;
